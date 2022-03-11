c3e8a881aaac054a59e84cf610ed0563f74f7ff7
MENGHEA <MENGHEA@MENG-PC>
2022-03-11 14:51:08 +0900

    Add project files.

---
+++ Hello World/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Hello_World
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string FirstFriend = "Alex";
+            string SecondFriend = "Menghea";
+            Console.WriteLine($"My Friend are {FirstFriend} and {SecondFriend}" );
+            Console.WriteLine();
+            // Find Length string
+            Console.WriteLine($"The name: {FirstFriend} has {FirstFriend.Length} letters.");
+            Console.WriteLine($"The name: {SecondFriend} has {SecondFriend.Length} letters.");
+        }
+    }
+}
